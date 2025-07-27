d51bfbbdb4bfa113dafdd0200c505545a86803fe
fworkneh7 <67432095+fworkneh7@users.noreply.github.com>
2025-07-27 00:54:01 -0400

    Delete pythonpractice.py

--- pythonpractice.py
+++
@@ -1,8 +0,0 @@
-f = open('inputFile.txt','r')
-passFile = open('PassFile.txt','w')
-for line in f:
-    line_split = line.split()
-    if line_split[2] == 'P':
-        print(line)
-f.close()
-passFile.close()
