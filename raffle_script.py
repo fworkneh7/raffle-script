5f57c82ad4c062337845d9801169e943374350de
fworkneh7 <67432095+fworkneh7@users.noreply.github.com>
2025-07-27 01:14:35 -0400

    Update raffle_script.py

--- raffle_script.py
+++ raffle_script.py
@@ -26,27 +26,3 @@ with open(path) as script_sheet:
         driver.execute_script("window.open('https://releases.ubiqlife.com/raffle/yeezy350black_nov29.html');")
         rowh = rowh+1
         driver.switch_to_window(driver.window_handles[rowh])
-
-
-
-
-# open gmail page
-# driver.get("https://gmail.com")
-
-# enter username
-# email_signin = driver.find_element_by_id("identifierId")
-# email_signin.send_keys("esahlemariam85@gmail.com")
-
-
-# go to password page
-# driver.find_element_by_xpath("//*[@id='identifierNext']/content/span").click()
-# time.sleep(2)
-
-# enter password
-# password_sumbit = driver.find_element_by_xpath("//*[@id='password']/div[1]/div/div[1]/input")
-# password_sumbit.send_keys("iluvfrazer")
-# time.sleep(2)
-
-# press submit for login
-# driver.find_element_by_xpath("//*[@id='passwordNext']/content/span").click()
-# time.sleep(5)
